569537adf22ace92ba5a3eabc0f822c4ebd8945c
Mohd Salman <salman0ansariii@gmail.com>
2022-10-04 23:54:17 +0530

    Update index.js
    body parser is deprecated

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express, { application } from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dashboard from './routes/dashboard.js';
@@ -12,14 +11,14 @@ dotenv.config();
 const CONNECTION_URL=process.env.MONGODB_URL;
 
 const PORT=process.env.PORT || 5000;
-app.use(bodyParser.json({extended:true}));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.text({extended:true}));
+app.use(express.json({extended:true}));
+app.use(express.urlencoded({extended:true}));
+app.use(express.text({extended:true}));
 app.use(cors());
 
-app.use("/dashboard/",dashboard);
-app.use("/jobs/",jobroute);
-app.use("/user/",user);
+app.use("/dashboard/", dashboard);
+app.use("/jobs/", jobroute);
+app.use("/user/", user);
 
 mongoose.connect(CONNECTION_URL,{ useNewUrlParser : true , useUnifiedTopology:true})
 .then(() => app.listen(PORT,() => console.log(`Server is running on port: ${PORT}`)))
